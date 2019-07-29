56c121970c6c28eba70d37e02aa66915b1a7cbae
Torenia.Founieri <kohmai.works@gmail.com>
2019-07-30 00:06:29 +0900

    Equipment update

---
+++ Assets/Script/01_Data/Equipments/Equipment.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Equip-", menuName = "Equip/Equipment", order = 1)]
+public class Equipment : ScriptableObject
+{
+    [SerializeField] public int EquipmentBaseID;
+
+    // Base Combat class parameter
+    // [TO DO] make an equipment class.
+
+    // Enhancement level -3 ~ 0, +1, +2 ~ +99
+    [SerializeField] public int EnhancementLevel;
+
+    //Prefix
+    // [TO DO] make a Prefix class.
+
+    //Suffix
+    // [TO DO] make a Suffix class.
+
+    //Enchantment slots, 0 to 4 (max)
+    // [TO DO] make an EnchantmentSlot class.
+    // which has fixed slot, random slot or no slot.
+    // EnchantSlotA
+    // EnchantSlotB
+    // EnchantSlotC
+    // EnchantSlotD
+
+
+}
