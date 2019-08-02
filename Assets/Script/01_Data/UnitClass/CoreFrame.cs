c3bc47bbe3320104c479be42095d318970bc4c03
Torenia.Founieri <kohmai.works@gmail.com>
2019-08-02 23:07:32 +0900

    UnitTypeScriptableObject
    proto- unit type

---
+++ Assets/Script/01_Data/UnitClass/CoreFrame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CoreFrame-", menuName = "Unit/CoreFrame", order = 3)]
+public class CoreFrame : ScriptableObject
+{
+    [SerializeField] public string Name;
+
+    [SerializeField] public FrameType FrameType;
+    [SerializeField] public TuningStype TuningStype;
+
+
+}
+
+
+public enum FrameType { twoLegs, spiderLegs, reverseJointLegs, caterpillar, wheel }
+public enum TuningStype { none, avoidTank, heavyTank, chargeMelee, swordsMelee, assaultMelee, rifleRenged, sinpeRenged, buff, debuff, assist, medic, supply }
+
+
+
---
+++ Assets/Script/01_Data/UnitClass/Pilot/Pilot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pilot-", menuName = "Unit/Pilot", order = 3)]
+public class Pilot : ScriptableObject
+{
+    public string Name;
+    public PilotStyle PilotStyle;
+    public Race Race;
+    public Preference Preference;
+
+
+}
+
+public enum PilotStyle { remoteControling , directBoarding, artificialIntelligence }
+public enum Race { human, AI }
+public enum Preference { offensive, defensive, obsessive}
+
+
+
