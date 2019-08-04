0b178c18787d026a300a0aa4af8a5c501f98169a
Torenia.Founieri <kohmai.works@gmail.com>
2019-08-04 23:28:25 +0900

    Caluculate battle unit design

--- Assets/Script/01_Data/UnitClass/UnitClass.cs
+++ Assets/Script/01_Data/UnitClass/UnitClass.cs
@@ -19,5 +19,64 @@ public class UnitClass : ScriptableObject
 
     [SerializeField] public CoreFrame CoreFrame;
     [SerializeField] public Pilot Pilot;
+    [SerializeField] public int Level;
+
+
+
+    // public AmplifyEquipmentRate AmplifyEquipmentRate
+
+    public BattleUnit CaluculatedBattleUnit()
+    {
+        //(1) Ability
+        // (1-1) Ability caluculation -> CaluculatedAbility
+        // Formula:
+        //  Ability = CoreFrame.Ability + Pilot.AddAbility + SummedItems.AddAbiliy
+
+        //(2) Combat
+        // (2-1) First Combat caluculation -> CombatRaw
+        // Formula:
+        //    CombatRaw.Shield = CoreFrame.Hitpoint (fixed, independent on Level)
+        //    CombatRaw.HitPoint = CoreFrame.Hitpoint (fixed, independent on Level)
+        //    CombatRaw.Others values: Level * Ability * coefficient(depend on others values)
+
+        // (2-3)Core Skill consideration, coreFrame skill and Pilot skill ->CombatBaseSkillConsidered
+        // Formula:
+        //    CombatCoreSkillConsidered.someValue = (CombatRaw.someValue + (CoreFrame and Pilot).skills.addCombat.someValue) * (CoreFrame and Pilot).skills.amplifyCombat.someValue
+
+        // (2-4)Skill consideration of items equiped -> CombatItemSkillConsidered
+        // Formula:
+        //    CombatItemSkillConsidered.someValue = (CombatCoreSkillConsidered.someValue + itemList.skills.addCombat.someValue) * itemList.skills.amplifyCombat.someValue
+
+        // (2-5)Add combat value of items equiped -> CombatItemEquiped
+        // Formula:
+        //    CombatItemEquiped.someValue = CombatItemSkillConsidered.someValue + itemList.addCombat.someValue * AmplifyEquipmentRate.someParts
+
+        // (2-6)Finalize
+        // Formula:
+        //    CombatCaluculated = CombatItemEquiped
+
+        //(3) Feature
+        //   double absorbShieldInitial, bool damageControlAssist, double hateInitial, double hateMagnificationPerTurn)
+        // Formula:
+        //   absorbShieldInitial = CoreFrame.addFeature.absorbShield + Pilot.addFeature.absorbShield
+        //   damageControlAssist = CoreFrame.TuningStype.damageControlAssist
+        //   hateInitial = (default value) + Pilot.addFeature.hate
+        //   hateMagnificationPerTurn = (defalut value) * Pilot.addFeature.hateMagnificationPerTurn
+
+        //(4)Offense/Defense/UnitSkill Magnification
+
+
+        //BattleUnit(int uniqueID, string name, Affiliation affiliation, UnitType unitType, AbilityClass ability, CombatClass combat, FeatureClass feature,
+        //OffenseMagnificationClass offenseMagnification, DefenseMagnificationClass defenseMagnification, UnitSkillMagnificationClass skillMagnification)
+
+        //BattleUnit battleUnit = new BattleUnit();
+
+        return null;
+    }
+    public AbilityClass CaluculatedAbility;
+    public CombatClass CombatCaluculated;
 
 }
+
+
+
