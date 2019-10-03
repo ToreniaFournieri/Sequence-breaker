cad50663a90f8bef9c86ebf58a5088f9995e4a61
Torenia.Founieri <kohmai.works@gmail.com>
2019-10-03 22:31:29 +0900

    Multi battle (doing)

---
+++ Assets/06_Script/08_Battle/BeforeBattle/EnemyUnitSet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyUnitSet-", menuName = "Unit/EnemyUnitSet", order = 3)]
+public class EnemyUnitSet : ScriptableObject
+{
+    public List<UnitClass> enemyUnitList;
+
+}
