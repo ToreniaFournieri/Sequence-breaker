91864b588f67e0c619a00da3b19762e27f0b87e4
Torenia.Founieri <kohmai.works@gmail.com>
2019-10-15 22:55:43 +0900

    updating character inventory..

---
+++ Assets/06_Script/03_Controller/01_Home/Character/CharacterTreeViewDataSourceMgr.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperScrollView;
+
+
+public class CharacterTreeViewItemData
+{
+    public string mName;
+    public string mIcon;
+    List<Item> mChildItemDataList = new List<Item>();
+
+    public int ChildCount
+    {
+        get { return mChildItemDataList.Count; }
+    }
+
+    public void AddChild(Item data)
+    {
+        mChildItemDataList.Add(data);
+    }
+    public Item GetChild(int index)
+    {
+        if (index < 0 || index >= mChildItemDataList.Count)
+        {
+            return null;
+        }
+        return mChildItemDataList[index];
+    }
+}
+
+public class CharacterTreeViewDataSourceMgr : MonoBehaviour
+{
+    // Item data base 
+    public ItemDataBase itemDataBase;
+    public List<Item> itemList;
+
+
+    List<CharacterTreeViewItemData> mItemDataList = new List<CharacterTreeViewItemData>();
+
+    static CharacterTreeViewDataSourceMgr instance = null;
+    int mTreeViewItemCount = 1;
+    //int mTreeViewChildItemCount = 10;
+
+
+    //int mTreeViewItemCount = 20;
+    //int mTreeViewChildItemCount = 30;
+
+    public static CharacterTreeViewDataSourceMgr Get
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Object.FindObjectOfType<CharacterTreeViewDataSourceMgr>();
+            }
+            return instance;
+        }
+
+    }
+
+    void Awake()
+    {
+        Init();
+    }
+
+
+    public void Init()
+    {
+        DoRefreshDataSource();
+    }
+
+    public CharacterTreeViewItemData GetItemDataByIndex(int index)
+    {
+        if (index < 0 || index >= mItemDataList.Count)
+        {
+            return null;
+        }
+        return mItemDataList[index];
+    }
+
+    public Item GetItemChildDataByIndex(int itemIndex, int childIndex)
+    {
+        CharacterTreeViewItemData data = GetItemDataByIndex(itemIndex);
+        if (data == null)
+        {
+            return null;
+        }
+        return data.GetChild(childIndex);
+    }
+
+    public int TreeViewItemCount
+    {
+        get
+        {
+            return mItemDataList.Count;
+        }
+    }
+
+    public int TotalTreeViewItemAndChildCount
+    {
+        get
+        {
+            int count = mItemDataList.Count;
+            int totalCount = 0;
+            for (int i = 0; i < count; ++i)
+            {
+                totalCount = totalCount + mItemDataList[i].ChildCount;
+            }
+            return totalCount;
+        }
+    }
+
+
+    void DoRefreshDataSource()
+    {
+        mItemDataList.Clear();
+        for (int i = 0; i < mTreeViewItemCount; ++i)
+        {
+            CharacterTreeViewItemData tData = new CharacterTreeViewItemData();
+            tData.mName = "Character";
+            //tData.mIcon = ResManager.Get.GetSpriteNameByIndex(Random.Range(0, 24));
+            tData.mIcon = "1";
+            mItemDataList.Add(tData);
+            //int childCount = mTreeViewChildItemCount;
+
+
+        foreach (Item _item in itemList)
+            {
+                tData.AddChild(_item);
+            }
+
+                //int childCount = itemDataBase.itemBaseMasterList.Count;
+                //for (int j = 0; j < childCount; ++j)
+                //{
+                //    Item _item = new Item();
+                //    _item.baseItem = itemDataBase.itemBaseMasterList[j];
+                //    //ItemData childItemData = new ItemData();
+                //    //childItemData.mName = "Item" + i + ":Child" + j;
+                //    //childItemData.mDesc = "Item Desc For " + childItemData.mName;
+                //    ////childItemData.mIcon = ResManager.Get.GetSpriteNameByIndex(Random.Range(0, 24));
+                //    //childItemData.mIcon = "1";
+                //    //childItemData.mStarCount = Random.Range(0, 6);
+                //    //childItemData.mFileSize = Random.Range(20, 999);
+                //    tData.AddChild(_item);
+                //}
+            //}
+
+        }
+    }
+
+
+
+}
+
---
+++ Assets/06_Script/03_Controller/01_Home/Inventory/layout/InventoryListItemContent.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SuperScrollView;
+using System;
+
+public class InventoryListItemContent : MonoBehaviour
+{
+    public Text mNameText;
+    public Image mIcon;
+
+    //public Image[] mStarArray;
+    //public Text mDescText;
+    public Text mDescriptionText;
+    //public Color32 mRedStarColor = new Color32(249, 227, 101, 255);
+    //public Color32 mGrayStarColor = new Color32(215, 215, 215, 255);
+    public GameObject mContentRootObj;
+
+    // Detail button = Detail Flag, handler
+    //public GameObject detailFlag;
+    // detail Flag click
+    Action<Item> mClickItemDetailHandler;
+    // button in detailFlag
+    public Button detailFlag;
+    public Item item;
+
+
+    // Show Item detail View
+    public GameObject itemDetailView;
+
+
+    int mItemDataIndex = -1;
+    int mChildDataIndex = -1;
+
+    public void Init()
+    {
+
+        //for (int i = 0; i < mStarArray.Length; ++i)
+        //{
+        //    int index = i;
+        //ClickEventListener listener = ClickEventListener.Get(detailFlag.gameObject);
+        //listener.SetClickEventHandler(delegate (GameObject obj) { OnButtonClicked(); });
+        //}
+
+        detailFlag.onClick.AddListener(OnButtonClicked);
+        //detailFlag.onClick.AddListener(OnButtonClicked());
+
+
+    }
+
+    public void SetClickCallBack(Action<Item> clickHandler)
+    {
+        mClickItemDetailHandler = clickHandler;
+    }
+
+    void OnButtonClicked()
+    {
+
+        if (mClickItemDetailHandler != null)
+        {
+
+            // Popup message set
+            Text _detailText = itemDetailView.transform.Find("ItemMiddleView/ItemDetailText").GetComponent<Text>();
+            _detailText.text = item.GetItemDetailDescription();
+            itemDetailView.SetActive(true);
+                
+
+        }
+
+    }
+
+
+    void OnStarClicked(int index)
+    {
+        ItemData data = TreeViewDataSourceMgr.Get.GetItemChildDataByIndex(mItemDataIndex, mChildDataIndex);
+        if (data == null)
+        {
+            return;
+        }
+        if (index == 0 && data.mStarCount == 1)
+        {
+            data.mStarCount = 0;
+        }
+        else
+        {
+            data.mStarCount = index + 1;
+        }
+        //SetStarCount(data.mStarCount);
+    }
+
+    //public void SetStarCount(int count)
+    //{
+    //    int i = 0;
+    //    for (; i < count; ++i)
+    //    {
+    //        mStarArray[i].color = mRedStarColor;
+    //    }
+    //    for (; i < mStarArray.Length; ++i)
+    //    {
+    //        mStarArray[i].color = mGrayStarColor;
+    //    }
+    //}
+
+    public void SetItemData(Item _item, int itemIndex, int childIndex)
+    {
+        mItemDataIndex = itemIndex;
+        mChildDataIndex = childIndex;
+        mNameText.text = _item.itemName;
+        //mDescText.text = itemData.mFileSize.ToString() + "KB";
+        mDescriptionText.text = _item.itemDescription;
+        //mIcon.sprite = null;
+
+        this.item = _item;
+        //mIcon.sprite = ResManager.Get.GetSpriteByName(itemData.mIcon);
+        //SetStarCount(itemData.mStarCount);
+    }
+
+
+}
+
