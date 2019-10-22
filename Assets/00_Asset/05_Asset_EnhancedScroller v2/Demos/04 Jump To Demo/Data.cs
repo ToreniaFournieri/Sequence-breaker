64b6f89dc83d656392457ed3def4f4a042d61077
Torenia.Founieri <kohmai.works@gmail.com>
2019-10-22 22:13:50 +0900

    deleting

--- Assets/00_Asset/05_Asset_EnhancedScroller v2/Demos/04 Jump To Demo/Data.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _00_Asset._05_Asset_EnhancedScroller_v2.Demos._04_Jump_To_Demo
-{
-    public class Data
-    {
-        public string CellText;
-    }
-}
--- Assets/00_Asset/05_Asset_EnhancedScroller v2/Demos/09 Cell Events/Data.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _00_Asset._05_Asset_EnhancedScroller_v2.Demos._09_Cell_Events
-{
-    public class Data
-    {
-        public int Hour;
-    }
-}
