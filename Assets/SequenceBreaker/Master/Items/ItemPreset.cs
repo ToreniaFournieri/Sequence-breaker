1bb69bb53e0b03f55a7047fe75bdb57461882737
Torenia.Founieri <torenia.fournieri@gmail.com>
2019-11-28 23:48:34 +0900

    itemPreset

---
+++ Assets/SequenceBreaker/Master/Items/ItemPreset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SequenceBreaker.Master.Items
+{
+    public class ItemPreset : ScriptableObject
+    {
+        public List<ItemIdSet> itemIdList;
+
+    }
+
+    public class ItemIdSet
+    {
+        public int PrifixId;
+        public int BaseId;
+        public int SuffixId;
+        public int enhancedValue;
+
+    }
+
+}
