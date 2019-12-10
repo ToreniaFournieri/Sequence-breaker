e1bedfabf431a79001a647b68741a96ce2ce821c
Torenia.Founieri <torenia.fournieri@gmail.com>
2019-12-10 21:21:34 +0900

    home list

---
+++ Assets/SequenceBreaker/Home/HomeListView/HomeListContents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SequenceBreaker.Master.UnitClass;
+using UnityEngine;
+
+namespace SequenceBreaker.Home.HomeListView
+{
+    [Serializable]
+    public class HomeListContentData
+    {
+        [SerializeField] public string contentText;
+        public string description;
+        public List<UnitClass> unitClassList;
+        public bool isInfinityInventoryMode;
+
+        //for SuperScroll use
+        public int mId;
+        public bool mChecked;
+        public bool mIsExpand;
+    }
+
+    [Serializable]
+    public class HomeListContents : MonoBehaviour
+    {
+
+        [SerializeField] public List<HomeListContentData> equipListContentList;
+
+    }
+
+}
