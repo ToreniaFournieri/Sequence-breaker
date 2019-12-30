2d550ee6340ee0c516865c42b172a2c3eb84ec65
Torenia.Founieri <torenia.fournieri@gmail.com>
2019-12-30 19:59:59 +0900

    words complete

--- Assets/SequenceBreaker/Translate/Word.cs
+++ Assets/SequenceBreaker/Translate/Word.cs
@@ -1,103 +1,54 @@
 ﻿using _00_Asset.I2.Localization.Scripts.Manager;
-using UnityEngine;
 
 namespace SequenceBreaker.Translate
 {
     public static class Word
     {
-
         public static string Get(string term)
         {
             string words = null;
             words = LocalizationManager.GetTranslation(term);
-
             if (words == null) { words = term; }
-
             return words;
-
         }
 
         public static string GetWithParam(string term, string value)
         {
-            string words = null;
-            words = LocalizationManager.GetTranslation(term);
+            string words = LocalizationManager.GetTranslation(term);
 
             if (words == null) { words = term; }
-
-
             //example) "plural X damages.[i2p_Zero] No damage.[i2p_One] 1 damage."
-            //Plural 0
-            int i2p_ZeroIndex = words.IndexOf("[i2p_Zero]", System.StringComparison.Ordinal);
-            int i2p_OneIndex = words.IndexOf("[i2p_One]", System.StringComparison.Ordinal);
-
-            Debug.Log("[i2p_Zero]: " + i2p_ZeroIndex + " i2p_OneIndex: " + i2p_OneIndex + " word: " + words);
+            int i2p_ZeroLength = words.IndexOf("[i2p_Zero]", System.StringComparison.Ordinal);
+            int i2p_OneLength = words.IndexOf("[i2p_One]", System.StringComparison.Ordinal);
 
             if (value == "0")
             {
-                if (i2p_OneIndex == -1)
+                if (i2p_OneLength == -1)
                 {
-                    //No special word exist, "plural X damages." should be selected.
-                    if (i2p_ZeroIndex == -1)
-                    {
-                        // no need to trim.
-                    }
-                    else
-                    {
-                        words = words.Substring(0, i2p_ZeroIndex);
-                    }
+                    if (i2p_ZeroLength == -1) { /* no need to trim. */  }
+                    else { words = words.Substring(0, i2p_ZeroLength); }
                 }
-                else
-                {
-                    // One index exist. use them.
-                    words = words.Substring(i2p_OneIndex + 9);
-                }
-
-
+                else { words = words.Substring(i2p_OneLength + 9); }
             }
             else if (value == "1")
             {
-                if (i2p_OneIndex == -1)
+                if (i2p_OneLength == -1)
                 {
-                    if (i2p_ZeroIndex == -1)
-                    {
-                        // no need to trim.
-                    }
-                    else
-                    {
-                        words = words.Substring(0, i2p_ZeroIndex);
-                    }
+                    if (i2p_ZeroLength == -1) { /* no need to trim. */  }
+                    else { words = words.Substring(0, i2p_ZeroLength); }
                 }
-                else
-                {
-                    words = words.Substring(0, words.Length - (i2p_OneIndex + 9));
-                }
+                else { words = words.Substring(0, words.Length - (i2p_OneLength + 9)); }
             }
             else
             {
-                if (i2p_ZeroIndex == -1)
+                if (i2p_ZeroLength == -1)
                 {
-                    if (i2p_OneIndex == -1)
-                    {
-                        // no need to trim
-                    }
-                    else
-                    {
-                        words = words.Substring(0, i2p_OneIndex);
-                    }
+                    if (i2p_OneLength == -1) { /* no need to trim */  }
+                    else { words = words.Substring(0, i2p_OneLength); }
                 }
-                else
-                {
-                    words = words.Substring(0, i2p_ZeroIndex);
-                }
+                else { words = words.Substring(0, i2p_ZeroLength); }
             }
-
-
-
-            if (words.Contains("{[VALUE]}"))
-            {
-                //Debug.Log("{[VALUE]} exist in :" + words + "param1 :" + value);
-                words = words.Replace("{[VALUE]}", value);
-            }
+            if (words.Contains("{[VALUE]}")) { words = words.Replace("{[VALUE]}", value); }
 
             return words;
         }
